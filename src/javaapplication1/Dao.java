c5b15f0ae1d8cca5bf1a2f004a542fd42caf3eb2
BuildTools <unconfigured@null.spigotmc.org>
2022-12-05 18:45:39 -0600

    Change tickets table name to support_tickets

--- src/javaapplication1/Dao.java
+++ src/javaapplication1/Dao.java
@@ -39,7 +39,7 @@ public class Dao {
 
 	public void createTables() {
 		// variables for SQL Query table creations
-		final String createTicketsTable = "CREATE TABLE jpapa_tickets(ticket_id INT AUTO_INCREMENT PRIMARY KEY, ticket_issuer VARCHAR(30), ticket_description VARCHAR(200))";
+		final String createTicketsTable = "CREATE TABLE support_tickets(ticket_id INT AUTO_INCREMENT PRIMARY KEY, ticket_issuer VARCHAR(30), ticket_description VARCHAR(200))";
 		final String createUsersTable = "CREATE TABLE jpapa_users(uid INT AUTO_INCREMENT PRIMARY KEY, uname VARCHAR(30), upass VARCHAR(30), admin int)";
 
 		try {
