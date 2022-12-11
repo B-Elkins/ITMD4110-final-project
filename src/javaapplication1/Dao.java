dd0b65cd4095c4e0ddff6432cbec1c2ede07303f
BuildTools <unconfigured@null.spigotmc.org>
2022-12-11 16:59:12 -0600

    Remove import for java.time.localDate

--- src/javaapplication1/Dao.java
+++ src/javaapplication1/Dao.java
@@ -8,7 +8,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
