8adcd358f2605192baf42901ce6654a8575ca558
BuildTools <unconfigured@null.spigotmc.org>
2022-12-11 18:05:23 -0600

    Remove import for java.sql.Connection

--- src/javaapplication1/Login.java
+++ src/javaapplication1/Login.java
@@ -3,7 +3,6 @@ package javaapplication1;
 import java.awt.GridLayout; //useful for layouts
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
